0f54aa297b2d6d094364f356f003e4e239d952fe
小螺号 <836419102@qq.com>
2020-05-02 17:16:42 +0800

    init

---
+++ src/components/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button type="primary">aaa</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+    data () {
+        return {
+
+        }
+    },
+
+    methods: {
+
+    }
+}
+
+</script>
+<style lang='less' scoped>
+</style>
